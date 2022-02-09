ab7cf62f3b3abf7191ef629492f2ec5acb3728a1
Hyun - Woo <nikel4610@gmail.com>
2022-02-09 14:31:00 +0900

    Update 02_Binary_search.py

--- Sequential_Search/02_Binary_search.py
+++ Sequential_Search/02_Binary_search.py
@@ -11,7 +11,7 @@ def binary_search(array, target, start, end):
         return binary_search(array, target, start, mid - 1)
     # 중간점의 값보다 찾고자 하는 값이 큰 경우 오른쪽 확인
     else:
-        return binary_search(array, target, start, mid + 1)
+        return binary_search(array, target, mid + 1, end)
 
 # n(원소의 개수)과 target(찾고자 하는 문자열)을 입력받기
 n ,target = list(map(int, input().split()))
@@ -24,3 +24,7 @@ if result == None:
     print("원소가 존재하지 않습니다")
 else:
     print(result + 1)
+
+# 10 7
+# 1 3 5 7 9 11 13 15 17 19
+# 4
